4d62303e3bb7ee30c300fa73e5229e8f63cc347e
galaxygamerman <mohdimadiqbal@gmail.com>
2024-12-11 13:20:16 +0530

    Removed App and put all routes into index.js

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -1,16 +1,24 @@
 import React from 'react';
-import {BrowserRouter} from 'react-router-dom';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import BasePage from './UsePages/BasePage';
+import Chef from './UsePages/Chef';
+import Waiter from './UsePages/Waiter';
+import OrderDetails from './UsePages/OrderDetails';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Router>
+    <Routes>
+      <Route path="/" element={<BasePage />} />
+      <Route path="/chef" element={<Chef />} />
+      <Route path="/waiter" element={<Waiter />} />
+      <Route path="/order/:id" element={<OrderDetails />} />
+    </Routes>
+  </Router>
 );
 
 // If you want to start measuring performance in your app, pass a function
