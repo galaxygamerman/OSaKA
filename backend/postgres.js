e9718159b0e28d153faa3c3ea44a474249057caf
galaxygamerman <mohdimadiqbal@gmail.com>
2024-12-26 23:12:03 +0530

    All the endpoints works now
    Promises require you to put brackets around them before you manipulate them

--- backend/postgres.js
+++ backend/postgres.js
@@ -24,8 +24,6 @@ pool.connect()
 
 // Create
 app.post('/item', async (req, res) => {
-	// console.log(req.body);
-
 	let name = req.body.name;
 	let items = req.body.items;
 	let total_price = req.body.total_price;
@@ -42,7 +40,7 @@ app.post('/item', async (req, res) => {
 				[customer_order_id, item.name, item.quantity, item.price]);
 			outputRows.push(...result2.rows);
 		}
-		console.log(outputRows);
+		// console.log(outputRows);
 		res.status(201).json(outputRows);
 	} catch (error) {
 		console.error("Error saving order", error);
@@ -58,14 +56,12 @@ app.get('/items', async (req, res) => {
 			: 'SELECT * FROM customer_order';
 		const values = req.query.status ? [req.query.status] : [];
 		let orders = (await pool.query(query, values)).rows;
-
 		orders = await Promise.all(orders.map(async order => {
 			const item = await pool.query('SELECT * FROM chosen_item WHERE order_id = $1', [order.id]);
 			order.items = item.rows; // Add items to the order
 			return order; // Return the modified order
 		}));
-		console.log(orders);
-		res.json(orders);
+		res.status(200).json(orders);
 	} catch (error) {
 		res.status(500).json({ message: error.message });
 	}
@@ -75,12 +71,11 @@ app.get('/items', async (req, res) => {
 app.get('/item/:id', async (req, res) => {
 	try {
 		let order = (await pool.query('SELECT * FROM customer_order WHERE id = $1', [req.params.id])).rows[0];
-
 		if (!order) return res.status(404).json({ message: 'Order not found' });
 		const items = await pool.query('SELECT * FROM chosen_item WHERE order_id = $1', [order.id]);
-		console.log(items);
+		// console.log(items);
 		order.items = items.rows;
-		res.json(order);
+		res.status(200).json(order);
 	} catch (error) {
 		res.status(500).json({ message: error.message });
 	}
@@ -89,9 +84,13 @@ app.get('/item/:id', async (req, res) => {
 // Update
 app.put('/item/:id/:newState', async (req, res) => {
 	try {
-		const result = await pool.query('UPDATE customer_order SET status = $1 WHERE id = $2 RETURNING *', [req.params.newState, req.params.id]);
-		if (result.rows.length === 0) return res.status(404).json({ message: 'Order not found' });
-		res.json(result.rows[0]);
+		const order = (await pool.query('UPDATE customer_order SET status = $1 WHERE id = $2 RETURNING *', [req.params.newState, req.params.id])).rows[0];
+
+		if (!order) return res.status(404).json({ message: 'Order not found' });
+
+		const items = await pool.query('SELECT * FROM chosen_item WHERE order_id = $1', [order.id]);
+		order.items = items.rows; // Add items to the order
+		res.status(200).json(order); // Return the updated order
 	} catch (error) {
 		res.status(400).json({ message: error.message });
 	}
