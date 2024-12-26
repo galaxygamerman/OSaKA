efcb5e3232576cd591de299698c7ee3d252bda14
galaxygamerman <mohdimadiqbal@gmail.com>
2024-12-26 21:19:40 +0530

    The get statements work

--- backend/postgres.js
+++ backend/postgres.js
@@ -42,12 +42,69 @@ app.post('/item', async (req, res) => {
 				[customer_order_id, item.name, item.quantity, item.price]);
 			outputRows.push(...result2.rows);
 		}
-
 		console.log(outputRows);
 		res.status(201).json(outputRows);
 	} catch (error) {
 		console.error("Error saving order", error);
 		res.status(400).json({ message: error.message });
+	}
+});
+
+// Read (all items)
+app.get('/items', async (req, res) => {
+	try {
+		const query = req.query.status
+			? 'SELECT * FROM customer_order WHERE status = $1'
+			: 'SELECT * FROM customer_order';
+		const values = req.query.status ? [req.query.status] : [];
+		let orders = (await pool.query(query, values)).rows;
+
+		orders = await Promise.all(orders.map(async order => {
+			const item = await pool.query('SELECT * FROM chosen_item WHERE order_id = $1', [order.id]);
+			order.items = item.rows; // Add items to the order
+			return order; // Return the modified order
+		}));
+		console.log(orders);
+		res.json(orders);
+	} catch (error) {
+		res.status(500).json({ message: error.message });
+	}
+});
+
+// Read (single item)
+app.get('/item/:id', async (req, res) => {
+	try {
+		let order = (await pool.query('SELECT * FROM customer_order WHERE id = $1', [req.params.id])).rows[0];
+
+		if (!order) return res.status(404).json({ message: 'Order not found' });
+		const items = await pool.query('SELECT * FROM chosen_item WHERE order_id = $1', [order.id]);
+		console.log(items);
+		order.items = items.rows;
+		res.json(order);
+	} catch (error) {
+		res.status(500).json({ message: error.message });
+	}
+});
+
+// Update
+app.put('/item/:id/:newState', async (req, res) => {
+	try {
+		const result = await pool.query('UPDATE customer_order SET status = $1 WHERE id = $2 RETURNING *', [req.params.newState, req.params.id]);
+		if (result.rows.length === 0) return res.status(404).json({ message: 'Order not found' });
+		res.json(result.rows[0]);
+	} catch (error) {
+		res.status(400).json({ message: error.message });
+	}
+});
+
+// Delete
+app.delete('/item/:id', async (req, res) => {
+	try {
+		const result = await pool.query('DELETE FROM customer_order WHERE id = $1 RETURNING *', [req.params.id]);
+		if (result.rows.length === 0) return res.status(404).json({ message: 'Order not found' });
+		res.json({ message: 'Order deleted successfully' });
+	} catch (error) {
+		res.status(500).json({ message: error.message });
 	}
 });
 
